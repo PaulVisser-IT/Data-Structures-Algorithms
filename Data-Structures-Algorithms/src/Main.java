dd4c25bf5d06c70d9d3252f44f471f99fd03c852
Paul Visser <paulvisser.it@gmail.com>
2022-06-23 11:59:28 +0200

    Added code the showcase O(n) and O(n^2) algorithms

--- Data-Structures-Algorithms/src/Main.java
+++ Data-Structures-Algorithms/src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        printItemsSquared(10);
+    }
+
+    /**
+     * Example of the O(n) Algorithm.
+     * @param n: times the for-lus runs.
+     */
+    public static void printItemsLineair(int n){
+        for(int i = 0; i < n; i++) {
+            System.out.println(i);
+        }
+    }
+
+    /**
+     * Example of the O(n^2) Algorithm
+     * n * n = O(n^2)
+     * @param n: times the for-lus runs.
+     */
+    public static void printItemsSquared(int n){
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                System.out.println(i + " " + j);
+            }
+        }
     }
 }
